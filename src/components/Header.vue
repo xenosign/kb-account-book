659e94cdbe1a82321189a40ad4121603f4e5e629
kdtTetz <xenosign@naver.com>
2024-06-06 05:16:14 +0900

    240606 세팅 시작

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <router-link to="/">home</router-link>
+    &nbsp;
+    <router-link to="/input">input</router-link>
+    &nbsp;
+    <router-link to="/show">show</router-link>
+    &nbsp;
+  </div>
+</template>
+
+<script setup></script>
