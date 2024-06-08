fcb2c8af5402dba7ff06f46161063879e113739d
xenosign <xenosign@naver.com>
2024-06-08 17:11:45 +0900

    240608 가계부 디자인 시작

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <header>
-    <Header />
-  </header>
-
-  <router-view />
+  <div class="container vh-90">
+    <header>
+      <Header />
+    </header>
+    <router-view />
+  </div>
 </template>
 
 <script setup>
 import Header from '@/components/Header.vue';
 </script>
+
+<style scoped>
+.vh-90 {
+  height: 90vh;
+}
+</style>
